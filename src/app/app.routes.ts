70dc09a2e1833f9d78f2365481f7dc0a4300045c
Farhat Samara <farhatsamara@gmail.com>
2026-02-12 14:54:58 -0600

    Added initial routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Login } from '../components/login/login';
+import { MainDashboard } from '../components/main-dashboard/main-dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'login',
+        component: Login,
+        title: 'Login'
+    },
+    {
+        path: 'main',
+        component: MainDashboard,
+        title: 'Home'
+    }
+
+];
